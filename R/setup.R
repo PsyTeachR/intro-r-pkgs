e451747bd714ee05eb3032d6b0b8c6b7a3db194e
debruine <debruine@gmail.com>
2023-04-25 15:40:49 +0100

    oh no the setup file is a mess now :(

--- R/setup.R
+++ R/setup.R
@@ -10,14 +10,16 @@ if (requireNamespace("glossary", quietly = TRUE)) {
 }
 
 # default knitr options
-knitr::opts_chunk$set(
-  echo       = TRUE,
-  warning    = FALSE,
-  message    = FALSE,
-  results    = "hold",
-  out.width  = '100%',
-  fig.width  = 8,
-  fig.height = 8,
-  fig.align  = 'center',
-  digits = 3
-)
+if (requireNamespace("knitr", quietly = TRUE)) {
+  knitr::opts_chunk$set(
+    echo       = TRUE,
+    warning    = FALSE,
+    message    = FALSE,
+    results    = "hold",
+    out.width  = '100%',
+    fig.width  = 8,
+    fig.height = 8,
+    fig.align  = 'center',
+    digits = 3
+  )
+}
